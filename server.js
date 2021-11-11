1f8b9399314c804d3c83a7e3de64096c7b9edef5
Anders Mørk <anders.mork@yahoo.com>
2021-11-11 14:58:39 +0100

    Update server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const server = express();
+const PORT = process.env.PORT || 8080;
+
+server.set("port", PORT);
+server.use(express.static("public"));
+server.use(express.json());
+
+server.get("/", (req, res, next) => {
+	res.status(200).send("Hello World").end();
+});
+
+server.post("/", (req, res, next) => {
+	res.status(200).end();
+});
+
+server.put("/", (req, res, next) => {
+	res.status(200).end();
+});
+
+server.delete("/", (req, res, next) => {
+	res.status(200).end();
+});
+
+server.listen(server.get("port"), function () {
+	console.log("server running", server.get("port"));
+});
