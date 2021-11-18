76a68667b22066490c20899e0e879a0eab9a6bec
alexandrawarholm <warholmalexandra@gmail.com>
2021-11-18 12:33:34 +0100

    router.delete user. håper det fungerer. oppdatert showToDoLists

--- modules/users.js
+++ modules/users.js
@@ -7,11 +7,51 @@ const router = express.Router();
 
 // user login ---------------------------------
 router.post("/users/login", async function(req, res, next){
-    res.status(200).send("Hello from POST - /users/login").end();
+    let credString = req.headers.authorization;
+    let cred = authUtils.decodeCred(credString);
+    
+    if (cred.username == "" || cred.password == "") {
+        res.status(401).json({error: "No username or password"}).end();
+        return;
+    } 
+	try {
+		let data = await db.getUser(cred.username);
+
+		if (data.rows.length > 0) {
+			let userid= data.rows[0].id;
+			let username = data.rows[0].username;
+			let password = data.rows[0].password;
+			let passwordHash = data.rows[0].hash;
+			let salt = data.rows[0].salt;
+
+			let isPasswordAccurate = authUtils.verifyPassword(cred.password, passwordHash, salt);
+
+			if(isPasswordAccurate == true) {
+				let tok = authUtils.createToken(username, userid);
+
+				res.status(200).json({
+					msg: "The login was successful!",
+					token:tok
+				}).end();
+
+			} else {
+				res.status(403).json({error: "Password not correct."}).end();
+			}
+		}
+		else {
+				res.status(403).json({error: "User doesnt exist."}).end();
+				return;
+		}		
+	}
+	catch(err) {
+		next(err);
+	}	
+
+
 });
 
 // list all users -------------------------------
-router.get("/users", async function(req, res, next){
+/*router.get("/users", async function(req, res, next){
    // res.status(200).send("Hello from GET - /users").end();
 
    try {
@@ -21,16 +61,79 @@ router.get("/users", async function(req, res, next){
    catch(err){
        next(err);
    }
-});
+});*/
 
 // create a new user -------------------------------
 router.post("/users", async function(req, res, next){
-    res.status(200).send("Hello from POST - /users").end();
+    let credString = req.headers.authorization;
+    let cred = authUtils.decodeCred(credString);
+    
+    if (cred.username == "" || cred.password == "") {
+        res.status(401).json({error: "No username or password"}).end();
+        return;
+    } 
+
+    let hash = authUtils.createHash(cred.password);
+    
+    try {
+		let data = await db.createUser(cred.username, hash.value, hash.salt);
+
+		if (data.rows.length > 0) {
+			res.status(200).json({msg: "The user was created succefully"}).end();
+		}
+		else {
+			throw "The user couldn't be created";
+		}		
+	}
+	catch(err) {
+		next(err);
+	}	
 });
 
 // delete a user -------------------------------
-router.delete("/users", async function(req, res, next){
-    res.status(200).send("Hello from DELETE - /users").end();
+router.delete("/users/delete", async function(req, res, next){
+    let credString = req.headers.authorization;
+    let cred = authUtils.decodeCred(credString);
+    
+    if (cred.username == "" || cred.password == "") {
+        res.status(401).json({error: "No username or password"}).end();
+        return;
+    } 
+	try {
+		let data = await db.getUser(cred.username);
+
+		if (data.rows.length > 0) {
+			let userid= data.rows[0].id;
+			let username = data.rows[0].username;
+			let password = data.rows[0].password;
+			let passwordHash = data.rows[0].hash;
+			let salt = data.rows[0].salt;
+
+			let isPasswordAccurate = authUtils.verifyPassword(cred.password, passwordHash, salt);
+
+			if(isPasswordAccurate == true) {
+				let tok = authUtils.createToken(username, userid);
+                
+                let waitForDeleting = await db.deleteUser(userid);
+
+				res.status(200).json({
+					msg: "You have deleted user successfully!",
+					token:tok
+				}).end();
+
+			} else {
+				res.status(403).json({error: "Password not correct."}).end();
+			}
+		}
+		else {
+				res.status(403).json({error: "User doesnt exist."}).end();
+				return;
+		}		
+	}
+	catch(err) {
+		next(err);
+	}	
+
 });
 
 //-----------------------------------------------
