71f6611aa9f7299f5154c1520e4b7a279fd9be15
stinehb <89643891+stinehb@users.noreply.github.com>
2021-11-18 11:37:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,25 +3,17 @@ const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 8080;
 
+const todolists = require("./modules/todolists.js");
+const users = require("./modules/users.js");
+
 server.set("port", PORT);
 server.use(express.static("public"));
 server.use(express.json());
 
-server.get("/", (req, res, next) => {
-	res.status(200).send("Hello World").end();
-});
+server.use(blogposts);
+server.use(users);
 
-server.post("/", (req, res, next) => {
-	res.status(200).end();
-});
 
-server.put("/", (req, res, next) => {
-	res.status(200).end();
-});
-
-server.delete("/", (req, res, next) => {
-	res.status(200).end();
-});
 
 server.listen(server.get("port"), function () {
 	console.log("server running", server.get("port"));
