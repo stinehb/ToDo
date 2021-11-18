234551c90165d1bcbc75a638bcf6cba174d041f6
alexandrawarholm <warholmalexandra@gmail.com>
2021-11-18 11:48:17 +0100

    auth for protect og auth_utils for verify password, create and check token og den tar seg av hash

---
+++ modules/auth.js
@@ -0,0 +1,23 @@
+const authUtils = require("./auth_utils.js");
+
+function protect(req,res,next){
+    let token = req.headers.authorization;
+
+    if(!token) {
+        res.status(401).json({error: "No token"}).end();
+        return;
+    }
+
+    let payload= authUtils.verifyToken(token);
+    if(!payload) {
+        res.status(403).json({error: "Not a valid token"}).end();
+        return;
+    }
+
+    res.locals.userid = payload.userid;
+    res.locals.username = payload.user;
+
+    next();
+}
+
+module.exports = protect;
--- modules/auth_utils.js
+++ modules/auth_utils.js
@@ -7,19 +7,72 @@ let utils = {};
 
 //--------------------------------------------
 utils.decodeCred = function(credString){
-    //code here
+    let cred = {};
+
+	let b64String = credString.replace("basic ", ""); // 'am9zdGVpbm46a29uZ29sYXY='	
+	let asciiString = Buffer.from(b64String, "base64").toString("ascii"); // 'josteinn:kongolav'	
+	cred.username = asciiString.replace(/:.*/, ""); //josteinn	
+	cred.password = asciiString.replace(cred.username + ":", ""); //kongolav
+
+	return cred;
 }
 //--------------------------------------------
 utils.createHash = function(password){
-    //code here
+    let hash = {};
+	
+	hash.salt = Math.random().toString();	
+	hash.value = crypto.scryptSync(password, hash.salt, 64).toString("hex");
+
+	return hash;
 }
 //--------------------------------------------
 utils.createToken = function(username, userID){
-    //code here
+    
+	let part1 = JSON.stringify({"alg": "HS256", "typ": "JWT"});
+	let part2 = JSON.stringify({"user": username, "userid": userID, "iat": Date.now()});
+	
+	let b64Part1 = Buffer.from(part1).toString("base64");
+	let b64Part2 = Buffer.from(part2).toString("base64");
+	
+	let openPart = b64Part1 + "." + b64Part2;	
+	
+	let sign = crypto.createHmac("SHA256", secret).update(openPart).digest("base64");
+
+	return openPart + "." + sign;
 }
 //--------------------------------------------
 utils.verifyToken = function(token){
-    //code here
+    let tokenArr = token.split(".");
+	let openPart = tokenArr[0] + "." + tokenArr[1];
+	let signToCheck = tokenArr[2];
+
+	let sign = crypto.createHmac("SHA256", secret).update(openPart).digest("base64");
+
+	if (signToCheck != sign) {		
+		//signature not ok
+		return false;
+	}
+	
+	let payloadTxt = Buffer.from(tokenArr[1], "base64").toString("ascii");
+	let payload = JSON.parse(payloadTxt);
+
+	let expireTime = payload.iat + 24 * 60 * 60 * 1000; //time in millisec.
+	if (expireTime < Date.now()) {
+		//the token has expired
+		return false;
+	} 
+	
+	//token ok
+	return payload;
 }
 //--------------------------------------------
+utils.verifyPassword = function(pswFromUser, hashFromDB, saltFromDB) {
+	hash = crypto.scryptSync(pswFromUser, saltFromDB, 64).toString("hex");
+
+	if(hash == hashFromDB) {
+		return true;
+	}
+	return false;
+}
+
 module.exports = utils;
