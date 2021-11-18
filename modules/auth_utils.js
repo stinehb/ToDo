0a8b30774a84af3d255648723ede06d834155c9d
stinehb <89643891+stinehb@users.noreply.github.com>
2021-11-18 11:10:59 +0100

    Create auth_utils.js

---
+++ modules/auth_utils.js
@@ -0,0 +1,25 @@
+const crypto = require ("crypto");
+
+//-------------------------------------------
+const secret = "dronningmaudsland";
+
+let utils = {};
+
+//--------------------------------------------
+utils.decodeCred = function(credString){
+    //code here
+}
+//--------------------------------------------
+utils.createHash = function(password){
+    //code here
+}
+//--------------------------------------------
+utils.createToken = function(username, userID){
+    //code here
+}
+//--------------------------------------------
+utils.verifyToken = function(token){
+    //code here
+}
+//--------------------------------------------
+module.exports = utils;
