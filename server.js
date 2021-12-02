be0e08b32fcee9633c31cce10f0ca930235fea1a
Anders Mørk <anders.mork@yahoo.com>
2021-12-02 17:14:10 +0100

    .

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const server = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const todolists = require("./modules/todolists.js");
 const users = require("./modules/users.js");
